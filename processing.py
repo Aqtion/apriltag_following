a6c4d0d0936cd9f2dd4b7a0b3cf7b811f34b4c65
Aqtion <keshavbadrinath06@gmail.com>
2023-08-01 16:10:29 -0400

    modularization 100

--- processing.py
+++ processing.py
@@ -1,9 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from dt_apriltags import Detector
 
-def process(frame, pid_x, pid_y, at_detector):
+def get_tags(frame, at_detector):
     dimensions = frame.shape
     height = dimensions[0]/2
     width = dimensions[1]/2
@@ -12,63 +11,116 @@ def process(frame, pid_x, pid_y, at_detector):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params = camera_params, tag_size=0.1)
     color_img = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+    return [tags, color_img]
+
+def draw_center(color_img):
+    dimensions = color_img.shape
+    height = dimensions[0]/2
+    width = dimensions[1]/2
     crosshair_offset = 40
     cv2.line(color_img, (int(width - crosshair_offset), int(height)), (int(width + crosshair_offset), int(height)), (255,0,0), 5)
     cv2.line(color_img, (int(width), int(height - crosshair_offset)), (int(width), int(height + crosshair_offset)), (255,0,0), 5)
-    # draw_tags(color_img, tags)
-    # plt.imshow(color_img)
+
+def process(frame, pid_x, pid_y, at_detector):
+    tags_img = get_tags(frame, at_detector)
+    tags = tags_img[0]
+    color_img = tags_img[1]
+
+    draw_center(color_img)
     try:
-        # print(tags)
-        x_error = 0
-        y_error = 0
-        center_x = 0
-        center_y = 0
-        for tag in tags:
-            for idx in range(len(tag.corners)):
-                cv2.line(color_img, tuple(tag.corners[idx - 1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0), 5)
-                translation_matrix = tag.pose_t.reshape(1,3)
-                # print(translation_matrix)
-                
-                x = translation_matrix[0][0]
-                y = translation_matrix[0][1]
-                z = translation_matrix[0][2]
+        data = get_errors(color_img, tags, True)
+        errors = data[0]
+        centers = data[1]
 
-                x_error += x
-                y_error += y
-                # output_video.write(color_img)
-            center_x += tag.center[0]
-            center_y += tag.center[1]
+        powers = get_powers(errors, pid_x, pid_y)
 
+        draw_powers(color_img, powers)
 
+        draw_tag_center(color_img, centers)
 
-        center_x = center_x / len(tags)
-        center_y = center_y / len(tags)
-        # print("x_err", center_x)
-        # print("y_err", center_y)
-        
-        crosshair_offset = 20
-        cv2.line(color_img, (int(center_x-crosshair_offset), int(center_y)), (int(center_x+crosshair_offset), int(center_y)), (0,0,255), 5)
-        cv2.line(color_img, (int(center_x), int(center_y-crosshair_offset)), (int(center_x), int(center_y+crosshair_offset)), (0,0,255), 5)
+        draw_line_to_center(color_img, centers)
 
-        cv2.line(color_img, (int(width), int(height)), (int(center_x), int(center_y)), (255,0,0), 5)
-        avg_x_error = x_error / len(tags)
-        avg_y_error = y_error / len(tags)
-        x_output = pid_x.update(avg_x_error)
-        y_output = pid_y.update(avg_y_error)
-        # print("x: ", x_output)
-        # print("y: ", y_output)
-
-        str_x_out = "x_output: " + str(x_output)
-        str_y_out = "y_output: " + str(y_output)
-
-        cv2.putText(color_img, str_x_out, (int(100),int(100)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 5, cv2.LINE_AA)
-
-        
-        cv2.putText(color_img, str_y_out, (int(1320),int(980)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 5, cv2.LINE_AA)
-
-        
-        p_img = color_img.copy()
-        return [x_output, y_output]
+        return [powers, color_img]
     except:
         pass
 
+def get_errors(color_img, tags, draw):
+    for tag in tags:
+        translation_matrix = tag.pose_t.reshape(1,3)
+            
+        x = translation_matrix[0][0]
+        y = translation_matrix[0][1]
+        # z = translation_matrix[0][2]
+
+        x_error += x
+        y_error += y
+        
+        center_x += tag.center[0]
+        center_y += tag.center[1]
+
+        if draw:
+            for idx in range(len(tag.corners)):
+                cv2.line(color_img, tuple(tag.corners[idx - 1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0), 5)
+
+    center_x = center_x / len(tags)
+    center_y = center_y / len(tags)
+    
+
+    avg_x_error = x_error / len(tags)
+    avg_y_error = y_error / len(tags)
+
+    return [[avg_x_error, avg_y_error], [center_x, center_y]]
+
+def draw_tag_center(color_img, centers):
+    dim = get_dimensions(color_img)
+    height = dim[0]/2
+    width = dim[1]/2
+
+    center_x = centers[0]
+    center_y = centers[1]
+
+    crosshair_offset = 20
+
+    cv2.line(color_img, (int(center_x-crosshair_offset), int(center_y)), (int(center_x+crosshair_offset), int(center_y)), (0,0,255), 5)
+    cv2.line(color_img, (int(center_x), int(center_y-crosshair_offset)), (int(center_x), int(center_y+crosshair_offset)), (0,0,255), 5)
+
+    cv2.line(color_img, (int(width), int(height)), (int(center_x), int(center_y)), (255,0,0), 5)
+
+
+def get_powers(errors, pid_x, pid_y):
+    x_error = errors[0]
+    y_error = errors[1]
+    x_output = pid_x.update(x_error)
+    y_output = pid_y.update(y_error)
+    return [x_output, y_output]
+
+def draw_powers(color_img, powers):
+    dim = get_dimensions(color_img)
+    height = dim[0]
+    width = dim[1]
+
+    x_output = powers[0]
+    y_output = powers[1]
+
+    str_x_out = "x_output: " + str(x_output)
+    str_y_out = "y_output: " + str(y_output)
+
+    x_side_offset = 100
+    top_offset = 100
+
+    y_side_offset = 500
+
+    cv2.putText(color_img, str_x_out, (int(x_side_offset),int(top_offset)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 5, cv2.LINE_AA) 
+    cv2.putText(color_img, str_y_out, (int(width - y_side_offset),int(top_offset)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 5, cv2.LINE_AA)
+
+def get_dimensions(color_img):
+    return color_img.shape
+
+def draw_line_to_center(color_img, centers):
+    dim = get_dimensions(color_img)
+    height = dim[0]/2
+    width = dim[1]/2
+
+    center_x = centers[0]
+    center_y = centers[1]
+    cv2.line(color_img, (int(width), int(height)), (int(center_x), int(center_y)), (255,0,0), 5)
